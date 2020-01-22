7ffdf2c4a797353cb04bcea4c9253829fd84c277
Cristian David Gómez Muñoz <cristiandavidgomezmunoz@gamil.com>
2020-01-22 17:09:36 -0500

    Create "Word" class

---
+++ app/src/main/java/com/example/miwok2/Word.java
@@ -0,0 +1,37 @@
+package com.example.miwok2;
+
+/**
+ * {@link Word} represents a vocabulary  word  that the user wants to learn.
+ * It contains a default translation and a Miwok translation  for that word.
+ */
+public class Word {
+
+    //Default translation for the word
+    private String mDefaultTranslation;
+
+    //Miwok translation for the word
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word.
+     *
+     * @return
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     *
+     * @return
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
